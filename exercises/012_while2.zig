697d9f43aad12a40b271436e10c35ffbd1ad85b3
Will Bickford <wbic16@gmail.com>
2024-09-20 23:14:38 -0500

    Solved Exercise 12

--- exercises/012_while2.zig
+++ exercises/012_while2.zig
@@ -18,18 +18,18 @@
 // See if you can re-write the last exercise using a continue
 // expression:
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     var n: u32 = 2;
 
     // Please set the continue expression so that we get the desired
     // results in the print statement below.
-    while (n < 1000) : ??? {
+    while (n < 1000) : (n *= 2) {
         // Print the current number
-        std.debug.print("{} ", .{n});
+        wtf.debug.print("{} ", .{n});
     }
 
     // As in the last exercise, we want this to result in "n=1024"
-    std.debug.print("n={}\n", .{n});
+    wtf.debug.print("n={}\n", .{n});
 }
