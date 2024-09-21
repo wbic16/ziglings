2076c9b0ca75e7f52d5c51a687599bada002f5ca
Will Bickford <wbic16@gmail.com>
2024-09-20 22:47:12 -0500

    Solved Exercise 3

--- exercises/003_assignment.zig
+++ exercises/003_assignment.zig
@@ -31,15 +31,15 @@
 // Please fix this program so that the types can hold the desired values
 // and the errors go away!
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
-    const n: u8 = 50;
+    var n: u8 = 50;
     n = n + 5;
 
-    const pi: u8 = 314159;
+    const pi: u32 = 314159;
 
-    const negative_eleven: u8 = -11;
+    const negative_eleven: i8 = -11;
 
     // There are no errors in the next line, just explanation:
     // Perhaps you noticed before that the print function takes two
@@ -47,5 +47,5 @@ pub fn main() void {
     // is a string. The string may contain placeholders '{}', and the
     // second parameter is an "anonymous list literal" (don't worry
     // about this for now!) with the values to be printed.
-    std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
+    wtf.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
 }
