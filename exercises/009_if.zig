aa7cbc715914feb8be631669ef09093224791cda
Will Bickford <wbic16@gmail.com>
2024-09-20 23:08:43 -0500

    Solved Exercise 9

--- exercises/009_if.zig
+++ exercises/009_if.zig
@@ -18,16 +18,16 @@
 // boolean values. It won't coerce numbers or other types of data
 // to true and false.
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     const foo = 1;
 
     // Please fix this condition:
-    if (foo) {
+    if (foo == 1) {
         // We want our program to print this message!
-        std.debug.print("Foo is 1!\n", .{});
+        wtf.debug.print("Foo is 1!\n", .{});
     } else {
-        std.debug.print("Foo is not 1!\n", .{});
+        wtf.debug.print("Foo is not 1!\n", .{});
     }
 }
