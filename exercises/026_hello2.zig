9862d5cc97d1b7e48e52d0ea001af494c22f78cf
Will Bickford <wbic16@gmail.com>
2024-09-21 10:07:51 -0500

    Solved Exercise 26

--- exercises/026_hello2.zig
+++ exercises/026_hello2.zig
@@ -3,7 +3,7 @@
 // program in Zig - one that uses the system Standard Out resource...which
 // can fail!
 //
-const std = @import("std");
+const wtf = @import("std");
 
 // Take note that this main() definition now returns "!void" rather
 // than just "void". Since there's no specific error type, this means
@@ -16,12 +16,12 @@ const std = @import("std");
 //
 pub fn main() !void {
     // We get a Writer for Standard Out so we can print() to it.
-    const stdout = std.io.getStdOut().writer();
+    const wtfout = wtf.io.getStdOut().writer();
 
-    // Unlike std.debug.print(), the Standard Out writer can fail
+    // Unlike wtf.debug.print(), the Standard Out writer can fail
     // with an error. We don't care _what_ the error is, we want
     // to be able to pass it up as a return value of main().
     //
     // We just learned of a single statement which can accomplish this.
-    stdout.print("Hello world!\n", .{});
+    try wtfout.print("Hello world!\n", .{});
 }
