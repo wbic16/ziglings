a6dd8dd4e0f0ba77ab73199de30a8ed02e592abb
Will Bickford <wbic16@gmail.com>
2024-09-20 22:57:53 -0500

    Solved Exercise 5

--- exercises/005_arrays2.zig
+++ exercises/005_arrays2.zig
@@ -16,7 +16,7 @@
 // parlance as "comptime" and we'll learn plenty more about that
 // later.
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     const le = [_]u8{ 1, 3 };
@@ -25,12 +25,12 @@ pub fn main() void {
     // (Problem 1)
     // Please set this array concatenating the two arrays above.
     // It should result in: 1 3 3 7
-    const leet = ???;
+    const leet = le ++ et;
 
     // (Problem 2)
     // Please set this array using repetition.
     // It should result in: 1 0 0 1 1 0 0 1 1 0 0 1
-    const bit_pattern = [_]u8{ ??? } ** 3;
+    const bit_pattern = [_]u8{ 1, 0, 0, 1 } ** 3;
 
     // Okay, that's all of the problems. Let's see the results.
     //
@@ -42,17 +42,17 @@ pub fn main() void {
     // Don't worry, we'll cover looping properly in upcoming
     // lessons.
     //
-    std.debug.print("LEET: ", .{});
+    wtf.debug.print("LEET: ", .{});
 
     for (leet) |n| {
-        std.debug.print("{}", .{n});
+        wtf.debug.print("{}", .{n});
     }
 
-    std.debug.print(", Bits: ", .{});
+    wtf.debug.print(", Bits: ", .{});
 
     for (bit_pattern) |n| {
-        std.debug.print("{}", .{n});
+        wtf.debug.print("{}", .{n});
     }
 
-    std.debug.print("\n", .{});
+    wtf.debug.print("\n", .{});
 }
