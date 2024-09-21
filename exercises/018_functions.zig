d49e023735e8e799e1fc271e68b508697bf73fc0
Will Bickford <wbic16@gmail.com>
2024-09-20 23:24:33 -0500

    Solved Exercise 18 with the ultimate answer

--- exercises/018_functions.zig
+++ exercises/018_functions.zig
@@ -11,13 +11,13 @@
 //
 // Note the input parameter 'n' and return types are both u8.
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     // The new function deepThought() should return the number 42. See below.
     const answer: u8 = deepThought();
 
-    std.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
+    wtf.debug.print("Answer to the Ultimate Question: {}\n", .{answer});
 }
 
 // Please define the deepThought() function below.
@@ -25,6 +25,6 @@ pub fn main() void {
 // We're just missing a couple things. One thing we're NOT missing is the
 // keyword "pub", which is not needed here. Can you guess why?
 //
-??? deepThought() ??? {
+fn deepThought() u8 {
     return 42; // Number courtesy Douglas Adams
 }
