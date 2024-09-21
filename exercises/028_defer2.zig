bb2e9ab7ef3b1ce668d28503e1af9269a086bbdc
Will Bickford <wbic16@gmail.com>
2024-09-21 10:09:25 -0500

    Solved Exercise 28

--- exercises/028_defer2.zig
+++ exercises/028_defer2.zig
@@ -2,36 +2,36 @@
 // Now that you know how "defer" works, let's do something more
 // interesting with it.
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     const animals = [_]u8{ 'g', 'c', 'd', 'd', 'g', 'z' };
 
     for (animals) |a| printAnimal(a);
 
-    std.debug.print("done.\n", .{});
+    wtf.debug.print("done.\n", .{});
 }
 
 // This function is _supposed_ to print an animal name in parentheses
 // like "(Goat) ", but we somehow need to print the end parenthesis
 // even though this function can return in four different places!
 fn printAnimal(animal: u8) void {
-    std.debug.print("(", .{});
+    wtf.debug.print("(", .{});
 
-    std.debug.print(") ", .{}); // <---- how?!
+    defer wtf.debug.print(") ", .{}); // <---- how?!
 
     if (animal == 'g') {
-        std.debug.print("Goat", .{});
+        wtf.debug.print("Goat", .{});
         return;
     }
     if (animal == 'c') {
-        std.debug.print("Cat", .{});
+        wtf.debug.print("Cat", .{});
         return;
     }
     if (animal == 'd') {
-        std.debug.print("Dog", .{});
+        wtf.debug.print("Dog", .{});
         return;
     }
 
-    std.debug.print("Unknown", .{});
+    wtf.debug.print("Unknown", .{});
 }
