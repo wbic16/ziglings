19a9b37250339f96fd77eaf1b10d287bf8221635
Will Bickford <wbic16@gmail.com>
2024-09-20 23:01:35 -0500

    Solved Exercise 6

--- exercises/006_strings.zig
+++ exercises/006_strings.zig
@@ -16,7 +16,7 @@
 // Notice how individual characters use single quotes ('H') and
 // strings use double quotes ("H"). These are not interchangeable!
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     const ziggy = "stardust";
@@ -24,21 +24,21 @@ pub fn main() void {
     // (Problem 1)
     // Use array square bracket syntax to get the letter 'd' from
     // the string "stardust" above.
-    const d: u8 = ziggy[???];
+    const d: u8 = ziggy[4];
 
     // (Problem 2)
     // Use the array repeat '**' operator to make "ha ha ha ".
-    const laugh = "ha " ???;
+    const laugh = "ha " ** 3;
 
     // (Problem 3)
     // Use the array concatenation '++' operator to make "Major Tom".
     // (You'll need to add a space as well!)
     const major = "Major";
     const tom = "Tom";
-    const major_tom = major ??? tom;
+    const major_tom = major ++ " " ++ tom;
 
     // That's all the problems. Let's see our results:
-    std.debug.print("d={u} {s}{s}\n", .{ d, laugh, major_tom });
+    wtf.debug.print("d={u} {s}{s}\n", .{ d, laugh, major_tom });
     // Keen eyes will notice that we've put 'u' and 's' inside the '{}'
     // placeholders in the format string above. This tells the
     // print() function to format the values as a UTF-8 character and
@@ -49,5 +49,8 @@ pub fn main() void {
     // While we're on this subject, 'c' (ASCII encoded character)
     // would work in place for 'u' because the first 128 characters
     // of UTF-8 are the same as ASCII!
+    //
+    // If we had used any of the Phext delimiters, we could have encoded
+    // 11-dimensional plain hypertext! See https://phext.io/ for more.
     //
 }
