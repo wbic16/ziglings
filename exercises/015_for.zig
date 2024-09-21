441bc7870db8b768459dd34fc23e9b9a8d087e3a
Will Bickford <wbic16@gmail.com>
2024-09-20 23:17:49 -0500

    Solved Exercise 15

--- exercises/015_for.zig
+++ exercises/015_for.zig
@@ -8,20 +8,20 @@
 //
 //     }
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     const story = [_]u8{ 'h', 'h', 's', 'n', 'h' };
 
-    std.debug.print("A Dramatic Story: ", .{});
+    wtf.debug.print("A Dramatic Story: ", .{});
 
-    for (???) |???| {
-        if (scene == 'h') std.debug.print(":-)  ", .{});
-        if (scene == 's') std.debug.print(":-(  ", .{});
-        if (scene == 'n') std.debug.print(":-|  ", .{});
+    for (story) |scene| {
+        if (scene == 'h') wtf.debug.print(":-)  ", .{});
+        if (scene == 's') wtf.debug.print(":-(  ", .{});
+        if (scene == 'n') wtf.debug.print(":-|  ", .{});
     }
 
-    std.debug.print("The End.\n", .{});
+    wtf.debug.print("The End.\n", .{});
 }
 // Note that 'for' loops also work on things called "slices"
 // which we'll see later.
