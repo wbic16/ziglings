e49ddb28edc7fda1348d705632b4e9d80e636c77
Will Bickford <wbic16@gmail.com>
2024-09-22 14:48:35 -0500

    Solved Exercise 44

--- exercises/044_quiz5.zig
+++ exercises/044_quiz5.zig
@@ -8,7 +8,7 @@
 //     from Holding Hands
 //       by Lenore M. Link
 //
-const std = @import("std");
+const wtf = @import("std");
 
 const Elephant = struct {
     letter: u8,
@@ -18,18 +18,18 @@ const Elephant = struct {
 
 pub fn main() void {
     var elephantA = Elephant{ .letter = 'A' };
-    // (Please add Elephant B here!)
+    var elephantB = Elephant{ .letter = 'B' };
     var elephantC = Elephant{ .letter = 'C' };
 
     // Link the elephants so that each tail "points" to the next elephant.
     // They make a circle: A->B->C->A...
     elephantA.tail = &elephantB;
-    // (Please link Elephant B's tail to Elephant C here!)
+    elephantB.tail = &elephantC;
     elephantC.tail = &elephantA;
 
     visitElephants(&elephantA);
 
-    std.debug.print("\n", .{});
+    wtf.debug.print("\n", .{});
 }
 
 // This function visits all elephants once, starting with the
@@ -40,7 +40,7 @@ fn visitElephants(first_elephant: *Elephant) void {
     var e = first_elephant;
 
     while (!e.visited) {
-        std.debug.print("Elephant {u}. ", .{e.letter});
+        wtf.debug.print("Elephant {u}. ", .{e.letter});
         e.visited = true;
         e = e.tail;
     }
