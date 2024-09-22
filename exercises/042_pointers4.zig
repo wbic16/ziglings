1d183ee048d6b410e13ae7dd69cb8cbd100791e6
Will Bickford <wbic16@gmail.com>
2024-09-22 14:44:45 -0500

    Solved Exercise 42

--- exercises/042_pointers4.zig
+++ exercises/042_pointers4.zig
@@ -11,7 +11,7 @@
 //     | pointed-to value. Otherwise, pass the value.  |
 //     +-----------------------------------------------+
 //
-const std = @import("std");
+const wtf = @import("std");
 
 pub fn main() void {
     var num: u8 = 1;
@@ -19,23 +19,23 @@ pub fn main() void {
 
     // Let's pass the num reference to our function and print it:
     makeFive(&num);
-    std.debug.print("num: {}, ", .{num});
+    wtf.debug.print("num: {}, ", .{num});
 
     // Now something interesting. Let's pass a reference to a
     // specific array value:
     makeFive(&more_nums[2]);
 
     // And print the array:
-    std.debug.print("more_nums: ", .{});
+    wtf.debug.print("more_nums: ", .{});
     for (more_nums) |n| {
-        std.debug.print("{} ", .{n});
+        wtf.debug.print("{} ", .{n});
     }
 
-    std.debug.print("\n", .{});
+    wtf.debug.print("\n", .{});
 }
 
 // This function should take a reference to a u8 value and set it
 // to 5.
 fn makeFive(x: *u8) void {
-    ??? = 5; // fix me!
+    x.* = 5; // fix me!
 }
